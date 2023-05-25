ddce0f4055a192b1ab12072677a8542bb704a2a8
achenguo <134472426+achenguo@users.noreply.github.com>
2023-05-25 12:24:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx22a0e6493cccbcfa",
+  appsecret: "1ff162b576f2b7e59be481d09593acd8",
+  template_id: "j4iPdKN9O2-9-fYArDN9pcSRNdXY2jrsyBb3FCj-1c4",
+  touser: "oZ4O65t0yB4udMriS0p3XmNTEosk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
+  city: "乌鲁木齐",
   first_date: "2022-11-10",
 };
